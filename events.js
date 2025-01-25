e2f0f27ea8dfb3311c0141496a3ab9cf5eb5a21f
Parthasarathi-c <cparthasarathi2023@gmail.com>
2025-01-25 23:44:56 +0530

    EVENTS IN JS

--- events.js
+++ events.js
@@ -9,8 +9,8 @@ x.on('sec',(...a) =>
         const para = a.join(' , ');
         console.log(`WE ARE ${para}`);
     });
-x.once('start',() => {console.log("STARTING...")} );
 
+x.once('start',() => {console.log("STARTING...")} );
 
 x.emit('start');
 x.emit('sec',1,2,3,4,5,6,7,8,9);
