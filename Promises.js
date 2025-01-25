22b1ba9e486699d4d26711f9918287b474584796
Parthasarathi-c <cparthasarathi2023@gmail.com>
2025-01-25 23:46:38 +0530

    PROMISES IN JS

--- Promises.js
+++ Promises.js
@@ -12,6 +12,7 @@ function suc(x)
      if( x % 2 == 0 ) { console.log(`${x} is a even n.o`);  }
     else { console.log(`${x} is an odd n.o`);  }
 }
+
 function fail(x)
 {
     console.log(`${x} is a negative n.o`)
